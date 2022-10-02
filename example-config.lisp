06a169eae876616a73e4bbcbaf4b991958db6dc3
Sahas Subramanian <sahas.subramanian@proton.me>
2022-10-02 13:55:13 +0200

    Improve example config

--- example-config.lisp
+++ example-config.lisp
@@ -1,3 +1,10 @@
+;; set active window opacity 1.0, inactive window opacity 0.9, and
+;; everytime a different window becomes active, take 200ms to
+;; gradually change opacity.
+(transitions 200 1 0.9)
+
+
+;; configure displays
 (displays
  ;; from left to right
  `(DP-3
@@ -7,7 +14,26 @@
    :scale 1.2
    :res "1920x1080"))
 
-;; set active window opacity 1.0, inactive window opacity 0.9, and
-;; everytime a different window becomes active, take 200ms to
-;; gradually change opacity.
-(transitions 200 1 0.9)
+
+;; or use named values to call `displays'
+(let* (
+       (fhd "1920x1080")
+       (qhd "3840x2160")
+
+       (laptop-screen
+        `(eDP-1))
+
+       (main-screen
+        `(HDMI-A-1))
+
+       (main-screen-fhd
+        `(HDMI-A-1
+          :scale 1.0
+          :res ,fhd))
+
+       (main-screen-qhd
+        `(HDMI-A-1
+          :scale 1.0
+          :res ,qhd)))
+
+  (displays main-screen laptop-screen))
