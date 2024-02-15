b87b0fccb1d8685504b7bba4a874b2d75305e0e8
Sahas Subramanian <sahas.subramanian@proton.me>
2024-02-15 16:24:51 +0100

    Use intern macro

--- src/outputs.rs
+++ src/outputs.rs
@@ -1,8 +1,29 @@
 use swayipc::Connection;
 
 use tulisp::{
-    lists::plist_get, tulisp_add_func, tulisp_fn, Error, ErrorKind, TulispContext, TulispObject,
+    intern, lists::plist_get, tulisp_add_func, tulisp_fn, Error, ErrorKind, TulispContext,
+    TulispObject,
 };
+
+intern! {
+    struct Keywords {
+        name: ":name",
+        make: ":make",
+        model: ":model",
+        serial: ":serial",
+        active: ":active",
+        resolution: ":resolution",
+        current_mode: ":current-mode",
+        refresh: ":refresh",
+        scale: ":scale",
+        pos_x: ":pos-x",
+        pos_y: ":pos-y",
+        width: ":width",
+        height: ":height",
+        modes: ":modes",
+        mode: ":mode",
+    }
+}
 
 pub(super) fn register_methods(ctx: &mut TulispContext) {
     tulisp_add_func!(ctx, get_outputs, "get-outputs");
@@ -25,75 +46,61 @@ fn get_outputs(ctx: &mut TulispContext) -> Result<TulispObject, Error> {
     })?;
     let results = TulispObject::nil();
 
-    let name = ctx.intern(":name");
-    let make = ctx.intern(":make");
-    let model = ctx.intern(":model");
-    let serial = ctx.intern(":serial");
-    let active = ctx.intern(":active");
-    let resolution = ctx.intern(":resolution");
-    let current_mode = ctx.intern(":current-mode");
-    let refresh = ctx.intern(":refresh");
-    let scale = ctx.intern(":scale");
-    let pos_x = ctx.intern(":pos-x");
-    let pos_y = ctx.intern(":pos-y");
-    let width = ctx.intern(":width");
-    let height = ctx.intern(":height");
-    let modes = ctx.intern(":modes");
-    let mode = ctx.intern(":mode");
+    let kw = Keywords::new(ctx);
 
     for output in outputs {
         results.push(tulisp::lists::plist_from([
-            (name.clone(), output.name.into()),
-            (make.clone(), output.make.into()),
-            (model.clone(), output.model.into()),
-            (serial.clone(), output.serial.into()),
-            (active.clone(), output.active.into()),
+            (kw.name.clone(), output.name.into()),
+            (kw.make.clone(), output.make.into()),
+            (kw.model.clone(), output.model.into()),
+            (kw.serial.clone(), output.serial.into()),
+            (kw.active.clone(), output.active.into()),
             (
-                resolution.clone(),
+                kw.resolution.clone(),
                 output
                     .current_mode
                     .map(|x| format!("{}x{}", x.width, x.height).into())
                     .unwrap_or_default(),
             ),
             (
-                current_mode.clone(),
+                kw.current_mode.clone(),
                 output
                     .current_mode
                     .map(|x| format!("{}x{}@{}", x.width, x.height, x.refresh as f64 / 1e3).into())
                     .unwrap_or_default(),
             ),
             (
-                refresh.clone(),
+                kw.refresh.clone(),
                 output
                     .current_mode
                     .map(|x| { x.refresh as f64 / 1e3 }.into())
                     .unwrap_or_default(),
             ),
             (
-                scale.clone(),
+                kw.scale.clone(),
                 output.scale.map(|x| x.into()).unwrap_or_default(),
             ),
-            (pos_x.clone(), (output.rect.x as i64).into()),
-            (pos_y.clone(), (output.rect.y as i64).into()),
-            (width.clone(), (output.rect.width as i64).into()),
-            (height.clone(), (output.rect.height as i64).into()),
+            (kw.pos_x.clone(), (output.rect.x as i64).into()),
+            (kw.pos_y.clone(), (output.rect.y as i64).into()),
+            (kw.width.clone(), (output.rect.width as i64).into()),
+            (kw.height.clone(), (output.rect.height as i64).into()),
             (
-                modes.clone(),
+                kw.modes.clone(),
                 output
                     .modes
                     .into_iter()
                     .map(|x| {
                         tulisp::lists::alist_from([
                             (
-                                mode.clone(),
+                                kw.mode.clone(),
                                 format!("{}x{}@{}", x.width, x.height, x.refresh as f64 / 1e3)
                                     .into(),
                             ),
                             (
-                                resolution.clone(),
+                                kw.resolution.clone(),
                                 format!("{}x{}", x.width, x.height).into(),
                             ),
-                            (refresh.clone(), (x.refresh as f64 / 1e3).into()),
+                            (kw.refresh.clone(), (x.refresh as f64 / 1e3).into()),
                         ])
                     })
                     .collect(),
@@ -105,19 +112,15 @@ fn get_outputs(ctx: &mut TulispContext) -> Result<TulispObject, Error> {
 
 #[tulisp_fn]
 fn set_output(ctx: &mut TulispContext, rest: TulispObject) -> Result<TulispObject, Error> {
-    let name = ctx.intern(":name");
-    let scale = ctx.intern(":scale");
-    let resolution = ctx.intern(":resolution");
-    let pos_x = ctx.intern(":pos-x");
-    let pos_y = ctx.intern(":pos-y");
+    let kw = Keywords::new(ctx);
 
     let args = rest.clone();
 
-    let tgt_name: Option<String> = plist_get(args.clone(), &name)?.try_into()?;
-    let tgt_scale: Option<f64> = plist_get(args.clone(), &scale)?.try_into()?;
-    let tgt_resolution: Option<String> = plist_get(args.clone(), &resolution)?.try_into()?;
-    let tgt_pos_x: Option<i64> = plist_get(args.clone(), &pos_x)?.try_into()?;
-    let tgt_pos_y: Option<i64> = plist_get(args.clone(), &pos_y)?.try_into()?;
+    let tgt_name: Option<String> = plist_get(&args, &kw.name)?.try_into()?;
+    let tgt_scale: Option<f64> = plist_get(&args, &kw.scale)?.try_into()?;
+    let tgt_resolution: Option<String> = plist_get(&args, &kw.resolution)?.try_into()?;
+    let tgt_pos_x: Option<i64> = plist_get(&args, &kw.pos_x)?.try_into()?;
+    let tgt_pos_y: Option<i64> = plist_get(&args, &kw.pos_y)?.try_into()?;
 
     if tgt_name.is_none() {
         return Err(Error::new(
@@ -174,7 +177,7 @@ fn set_output(ctx: &mut TulispContext, rest: TulispObject) -> Result<TulispObjec
     get_outputs(ctx)?
         .base_iter()
         .find(|x| {
-            plist_get(x.clone(), &name)
+            plist_get(x, &kw.name)
                 .map(|x| tgt_name == x.try_into().ok())
                 .unwrap_or(false)
         })
